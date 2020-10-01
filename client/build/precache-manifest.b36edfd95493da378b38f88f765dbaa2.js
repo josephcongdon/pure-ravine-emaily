49936f56c26d78bc9ff761095164689163e3b4ca
joe_congdon <narniageek92@yahoo.com>
2020-09-30 20:42:15 -0500

    debugging

---
+++ client/build/precache-manifest.b36edfd95493da378b38f88f765dbaa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9144bc25551f0ed7904c7bed88b3388a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001de8fa3773b531e75a",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "001de8fa3773b531e75a",
+    "url": "/static/js/2.6cd7781a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6cd7781a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd28d1eda02e2bb0d665",
+    "url": "/static/js/main.5175ac49.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
