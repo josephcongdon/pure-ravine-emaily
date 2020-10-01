4f15b7ee377acba5725bfae9c79de6e6c39181ab
joe_congdon <narniageek92@yahoo.com>
2020-09-30 22:17:07 -0500

    I hope this works

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+//dev.js - dont commit this
+
+module.exports = {
+    googleClientID: '983403777825-i26pfbrheirvjbebisq3h9c7nos2q83q.apps.googleusercontent.com',
+    googleClientSecret: 'kRmRa6TJEg5xJSohvI3n4qUA',
+    mongoURI: 'mongodb+srv://jocongdon:LTh6czPzNmYQgPd1@emaily.kgeys.mongodb.net/joco?retryWrites=true&w=majority',
+    cookieKey: 'svbassvhjbsvvbassvjkbassvhjb',
+    stripePublishableKey: 'pk_test_51HTtFMBP0pje228CVHhDspzBIPuZ24mjx8od6Ps3XdhmoqA3LlJivDwV9kPqTVYieBKngLgGvTxTwGxEjWclufEx0057FtBDQ0',
+    stripeSecretKey:'sk_test_51HTtFMBP0pje228CQnDsUe8CtlX15ULM32G8XvwSvOhidB4eO8rB9qNJbnGthHntXDoaRbhGuDlW6GgogonLsGQG00KG2c38Ci'
+};
